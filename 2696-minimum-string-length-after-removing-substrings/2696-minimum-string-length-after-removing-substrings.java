29cb301b764d674cb8787a86001d938704e14511
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-08-01 17:59:10 +0530

    Time: 5 ms (63.04%), Space: 45.1 MB (23.23%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -2,19 +2,14 @@ class Solution {
     public int minLength(String s) {
         Stack<Character> st=new Stack<>();
         for(char ch:s.toCharArray()){
-            if(!st.isEmpty()){
-                if(ch=='B' && st.peek()=='A'){
-                    st.pop();
-                }
-                else if(ch=='D' && st.peek()=='C'){
-                    st.pop();
-                }
-                else{
-                    st.push(ch);
-                }
+            if(!st.isEmpty() && ch=='B' && st.peek()=='A'){
+                st.pop();
+            }
+            else if(!st.isEmpty() && ch=='D' && st.peek()=='C'){
+                st.pop();
             }
             else{
-                st.push(ch);
+            st.push(ch);
             }
         }
         return st.size();
