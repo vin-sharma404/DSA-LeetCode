f6434a17ca1e1d1432558ce44da61ed68c1324a8
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-08-02 18:07:34 +0530

    Time: 3 ms (86.07%), Space: 43.6 MB (10.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        Map<Character, Character> map = new HashMap<>();
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
-
-        for(char ch : s.toCharArray()){
-            if(!map.containsKey(ch)){      
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
                 st.push(ch);
             }
-            else{                          
-                if(st.isEmpty() || st.pop()!=map.get(ch))
+            else if(ch==')'){
+                if(st.isEmpty() || st.pop()!='('){
                     return false;
+                }
+            }
+            else if(ch=='}'){
+                if(st.isEmpty() || st.pop()!='{'){
+                    return false;
+                }
+            }
+            else if(ch==']'){
+                if(st.isEmpty() || st.pop()!='['){
+                    return false;
+                }
             }
         }
-
-        return st.isEmpty();
+       return st.isEmpty();
     }
 }
