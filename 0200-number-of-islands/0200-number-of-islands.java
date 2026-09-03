25fea43e38a20735cacc96072aae0308dc39531c
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-09-03 19:33:51 +0530

    Time: 5 ms (29.29%), Space: 54.7 MB (5.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,62 +1,35 @@
 class Solution {
     int rows;
     int cols;
-
     public int numIslands(char[][] grid) {
-        rows = grid.length;
-        cols = grid[0].length;
-
-        int islands = 0;
+        rows=grid.length;
+        cols=grid[0].length;
+        int islands=0;
         boolean[][] visited = new boolean[rows][cols];
-
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-
-                if(grid[i][j] == '1' && !visited[i][j]) {
-                    bfs(i, j, grid, visited);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    dfs(i,j,grid,visited);
                     islands++;
                 }
             }
         }
-
         return islands;
     }
 
-    public void bfs(int row, int col, char[][] grid, boolean[][] visited) {
-
-        Queue<int[]> q = new LinkedList<>();
-
-        q.offer(new int[]{row, col});
-        visited[row][col] = true;
-
-        int[][] directions = {
-            {-1, 0},
-            {0, 1},
-            {1, 0},
-            {0, -1}
+    public void dfs(int row,int col,char[][] grid, boolean[][] visited){
+        if(row<0 || row>=rows || col<0 || col>=cols || grid[row][col] == '0' || visited[row][col]){
+            return;
+        }
+        visited[row][col]=true;
+        int[][] adjList= {
+            {row-1,col},
+            {row,col+1},
+            {row+1,col},
+            {row,col-1}
         };
-
-        while(!q.isEmpty()) {
-
-            int[] current = q.poll();
-
-            int currRow = current[0];
-            int currCol = current[1];
-
-            for(int[] dir : directions) {
-
-                int newRow = currRow + dir[0];
-                int newCol = currCol + dir[1];
-
-                if(newRow >= 0 && newRow < rows &&
-                   newCol >= 0 && newCol < cols &&
-                   grid[newRow][newCol] == '1' &&
-                   !visited[newRow][newCol]) {
-
-                    visited[newRow][newCol] = true;
-                    q.offer(new int[]{newRow, newCol});
-                }
-            }
+        for(int[] neighbour : adjList){
+            dfs(neighbour[0],neighbour[1],grid,visited);
         }
     }
 }
