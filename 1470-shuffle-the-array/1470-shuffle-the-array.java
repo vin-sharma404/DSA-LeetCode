05d71044d50ed541e2a8d86fe20c7dad3e78cd48
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-09-08 00:28:11 +0530

    Time: 0 ms (100%), Space: 46.7 MB (22.39%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] res= new int[2*n];
-        int i=0;
-        for(int j=0;j<n;j++){
-            res[i++]=nums[j];
-            res[i++]=nums[j+n];
+        int[]res = new int[2*n];
+        int j=0;
+        for(int i=0;i<n;i++){
+            res[j++] = nums[i];
+            res[j++] = nums[i+n];
         }
         return res;
-
     }
 }
