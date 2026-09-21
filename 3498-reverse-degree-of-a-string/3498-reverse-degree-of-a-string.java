ae8d586d10aa963dc508f308ac6c9638c98e6eac
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-09-22 00:35:17 +0530

    Time: 1 ms (100%), Space: 43.9 MB (80.81%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -3,7 +3,6 @@ class Solution {
         int sum=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            // sum+=(i+1)*(Math.abs(ch-'z')+1);
             int val=Math.abs(ch-'z');
             sum+=(i+1)*(val+1);
         }
