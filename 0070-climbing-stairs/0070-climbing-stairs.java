ce42e734887dddc9f9cb5e15c6a3216743ceaa9d
vin-sharma404 <vinayak2004sharma@gmail.com>
2026-09-23 00:21:37 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -6,7 +6,7 @@ class Solution {
         int prev1=1;
         int prev2=2;
         for(int i=3;i<=n;i++){
-            int temp=prev1+prev2;
+            int temp= prev1+prev2;
             prev1=prev2;
             prev2=temp;
         }
